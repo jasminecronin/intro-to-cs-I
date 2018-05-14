ded1af1f38db2e38cb04b1957845592dab5dcfce
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-14 09:19:01 -0600

    Create 2.4.1.rkt

---
+++ Extra Credit/HtDP/2.4.1.rkt
@@ -0,0 +1,8 @@
+(+ (10) 20)
+function call: expected a function after the open parenthesis, but found a number
+
+(10 + 20)
+function call: expected a function after the open parenthesis, but found a number
+
+(+ +)
++: expected a function call, but there is no open parenthesis before this function
