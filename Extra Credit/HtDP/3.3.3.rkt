8d732b27f4f2f9d6f54a597464a1d7f42a703a67
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-16 05:51:48 -0600

    Update 3.3.3.rkt

--- Extra Credit/HtDP/3.3.3.rkt
+++ Extra Credit/HtDP/3.3.3.rkt
@@ -1,6 +1,14 @@
+;; area-cylinder: number number -> number
+;; Calculates surface area of a cylinder given base radius and height
 (define (area-cylinder r h)
   (+ (* 2 (area-circle r)) (* h (circumference r))))
+  
+;; area-circle: number -> number
+;; calculates area of a circle given its radius
 (define (area-circle r)
   (* r r pi))
+  
+;; circumference: number -> number
+;; calculates circumference of a circle given its radius
 (define (circumference r)
   (* 2 pi r))
