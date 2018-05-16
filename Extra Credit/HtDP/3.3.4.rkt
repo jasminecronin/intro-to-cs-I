66c6c57541d9c277e9b6aab2dcfaed44f141c2a9
Jasmine Roebuck <35498193+roebuckj3@users.noreply.github.com>
2018-05-16 06:00:54 -0600

    Update 3.3.4.rkt

--- Extra Credit/HtDP/3.3.4.rkt
+++ Extra Credit/HtDP/3.3.4.rkt
@@ -1,1 +1,16 @@
+;; area-pipe: number number number -> number
+;; to find the surface area of an open cylinder given an inner radius, length, and wall thickness
+(define (area-pipe radius length thickness)
+  (+ (* (circumference radius) length)
+     (* (circumference (+ radius thickness)) length)
+     (* 2 (- (area-circle (+ radius thickness)) (area-circle radius)))))
 
+;; circumference: number -> number
+;; to find the circumference of a circle given a radius
+(define (circumference radius)
+  (* 2 pi radius))
+
+;; area-circle: number -> number
+;; to find the area of a circle given a radius
+(define (area-circle radius)
+  (* pi radius radius))
